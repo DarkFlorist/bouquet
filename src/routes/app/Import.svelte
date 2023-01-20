819cf1149be30329c9ebdeb7ccb97fc26420fb60
0xjimmy <contact@decentralised.tech>
2023-01-20 17:04:08 +1300

    Check for errors not finding compatible wallets for importing

--- src/routes/app/Import.svelte
+++ src/routes/app/Import.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang='ts'>
 	import Button from '$lib/components/Button.svelte'
 	import {
 		bundleTransactions,
@@ -15,35 +15,36 @@
 	import { Wallet } from 'ethers'
 	import type { FlashbotsBundleTransaction } from '@flashbots/ethers-provider-bundle'
 
+	let importError = '' 
+
+	// @TODO: Doesn't check for case of simulation with no transactions
 	async function importFromInterceptor() {
-		if (window.ethereum === undefined) return
-		// @ts-ignore
-		await window.ethereum.request({ method: 'eth_requestAccounts' })
-		// @ts-ignore
-		const { payload } = (await window.ethereum.request({
-			method: 'interceptor_getSimulationStack',
-		})) as { payload: PayloadTransaction[] }
+		try {
+			if (window.ethereum === undefined) throw 'No Wallet'
+			// @ts-ignore
+			await window.ethereum.request({ method: 'eth_requestAccounts' })
+			// @ts-ignore
+			const { payload } = (await window.ethereum.request({
+				method: 'interceptor_getSimulationStack',
+			})) as { payload: PayloadTransaction[] }
 
-		console.log({ payload })
+			const uniqueSigningAccounts = [...new Set(payload.map((tx) => tx.from))]
+			const isFundingTransaction = payload.length >= 2 && uniqueSigningAccounts.includes(payload[0].to)
 
-		const uniqueSigningAccounts = [...new Set(payload.map((tx) => tx.from))]
-		const isFundingTransaction =
-			payload.length >= 2 && uniqueSigningAccounts.includes(payload[0].to)
+			const transactions = payload.map(
+				({ from, to, value, input, gas }) => ({
+					transaction: { from, to, value, data: input, gasLimit: gas },
+				})
+			) as FlashbotsBundleTransaction[]
 
-		const transactions = payload.map(
-			({ from, to, value, input, gas }) => ({
-				transaction: { from, to, value, data: input, gasLimit: gas },
-			})
-		) as FlashbotsBundleTransaction[]
-
-		let fundingTarget: string
-		if (isFundingTransaction) {
-			if ($wallets.length === 0) {
-				wallets.update((x) => [...x, Wallet.createRandom()])
-			}
-			fundingTarget = payload[0].to
-			uniqueSigningAccounts.shift()
-			transactions.shift()
+			let fundingTarget: string
+			if (isFundingTransaction) {
+				if ($wallets.length === 0) {
+			    wallets.update((x) => [...x, Wallet.createRandom()])
+				}
+				fundingTarget = payload[0].to
+				uniqueSigningAccounts.shift()
+				transactions.shift()
 		}
 
 		totalGas.set(transactions.reduce(
@@ -56,9 +57,9 @@
 			.filter((tx) => tx.transaction.from === fundingTarget)
 			.reduce(
 				(sum, current) => sum + BigInt(current.transaction.value?.toString() ?? 0n),
-        0n
+				0n
 			)
-    )
+		)
 
 		localStorage.setItem('payload', JSON.stringify(payload))
 		interceptorPayload.set(payload)
@@ -66,6 +67,18 @@
 		uniqueSigners.set(uniqueSigningAccounts)
 		bundleTransactions.set(transactions)
 		bundleContainsFundingTx.set(isFundingTransaction)
+
+		} catch (err: any) {
+			if (err === 'No Wallet') {
+				importError = 'Import Error: No Ethereum wallet detected'
+			} else if (err?.code === 4001) {
+				importError = 'Import Error: Wallet connection rejected'
+			} else if (err?.code === -32601) {
+				importError = 'Import Error: Wallet does not support returning simulations'
+			} else {
+				importError = `Unknown Error: ${JSON.stringify(err)}`
+			}
+		}
 	}
 </script>
 
@@ -78,5 +91,11 @@
 		<Button onClick={importFromInterceptor}
 			>Import Payload from TheInterceptor</Button
 		>
+		{#if importError}
+			<span>{importError}</span>
+			{#if importError === 'Import Error: Wallet does not support returning simulations'}
+				<h3 class="text-xl">Don't have TheInterceptor Installed? Install it here <a class="font-bold hover:underline" href="https://dark.florist">here</a>.</h3>
+			{/if}
+		{/if}
 	</div>
 </article>
