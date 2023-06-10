3aceeaa662397efbe2fbf8096c96d1881c1d953d
0xjimmy <me@0xjimmy.xyz>
2023-06-10 13:53:53 +1200

    Fix weird formatting

--- app/ts/types/interceptorTypes.ts
+++ app/ts/types/interceptorTypes.ts
@@ -13,67 +13,59 @@ type EthereumAccessList = t.Static<typeof EthereumAccessList>
 
 type EthereumUnsignedTransactionLegacy = t.Static<typeof EthereumUnsignedTransactionLegacy>
 const EthereumUnsignedTransactionLegacy = t.Intersect(
-	t
-		.Object({
-			type: t.Union(
-				t.Literal('0x0').withParser(LiteralConverterParserFactory('0x0', 'legacy' as const)),
-				t.Literal(undefined).withParser(LiteralConverterParserFactory(undefined, 'legacy' as const)),
-			),
-			from: EthereumAddress,
-			nonce: EthereumQuantity,
-			gasPrice: EthereumQuantity,
-			gas: EthereumQuantity,
-			to: t.Union(EthereumAddress, t.Null),
-			value: EthereumQuantity,
-			input: EthereumInput,
-		})
-		.asReadonly(),
-	t
-		.Partial({
-			chainId: EthereumQuantity,
-		})
-		.asReadonly(),
+	t.Object({
+		type: t.Union(
+			t.Literal('0x0').withParser(LiteralConverterParserFactory('0x0', 'legacy' as const)),
+			t.Literal(undefined).withParser(LiteralConverterParserFactory(undefined, 'legacy' as const)),
+		),
+		from: EthereumAddress,
+		nonce: EthereumQuantity,
+		gasPrice: EthereumQuantity,
+		gas: EthereumQuantity,
+		to: t.Union(EthereumAddress, t.Null),
+		value: EthereumQuantity,
+		input: EthereumInput,
+	}).asReadonly(),
+	t.Partial({
+		chainId: EthereumQuantity,
+	}).asReadonly()
 )
 
 type EthereumUnsignedTransaction2930 = t.Static<typeof EthereumUnsignedTransaction2930>
 const EthereumUnsignedTransaction2930 = t.Intersect(
-	t
-		.Object({
-			type: t.Literal('0x1').withParser(LiteralConverterParserFactory('0x1', '2930' as const)),
-			from: EthereumAddress,
-			nonce: EthereumQuantity,
-			gasPrice: EthereumQuantity,
-			gas: EthereumQuantity,
-			to: t.Union(EthereumAddress, t.Null),
-			value: EthereumQuantity,
-			input: EthereumInput,
-			chainId: EthereumQuantity,
-		})
-		.asReadonly(),
+	t.Object({
+		type: t.Literal('0x1').withParser(LiteralConverterParserFactory('0x1', '2930' as const)),
+		from: EthereumAddress,
+		nonce: EthereumQuantity,
+		gasPrice: EthereumQuantity,
+		gas: EthereumQuantity,
+		to: t.Union(EthereumAddress, t.Null),
+		value: EthereumQuantity,
+		input: EthereumInput,
+		chainId: EthereumQuantity,
+	}).asReadonly(),
 	t.Partial({
 		accessList: EthereumAccessList,
-	}),
+	})
 )
 
 type EthereumUnsignedTransaction1559 = t.Static<typeof EthereumUnsignedTransaction1559>
 const EthereumUnsignedTransaction1559 = t.Intersect(
-	t
-		.Object({
-			type: t.Literal('0x2').withParser(LiteralConverterParserFactory('0x2', '1559' as const)),
-			from: EthereumAddress,
-			nonce: EthereumQuantity,
-			maxFeePerGas: EthereumQuantity,
-			maxPriorityFeePerGas: EthereumQuantity,
-			gas: EthereumQuantity,
-			to: t.Union(EthereumAddress, t.Null),
-			value: EthereumQuantity,
-			input: EthereumInput,
-			chainId: EthereumQuantity,
-		})
-		.asReadonly(),
+	t.Object({
+		type: t.Literal('0x2').withParser(LiteralConverterParserFactory('0x2', '1559' as const)),
+		from: EthereumAddress,
+		nonce: EthereumQuantity,
+		maxFeePerGas: EthereumQuantity,
+		maxPriorityFeePerGas: EthereumQuantity,
+		gas: EthereumQuantity,
+		to: t.Union(EthereumAddress, t.Null),
+		value: EthereumQuantity,
+		input: EthereumInput,
+		chainId: EthereumQuantity,
+	}).asReadonly(),
 	t.Partial({
 		accessList: EthereumAccessList,
-	}),
+	})
 )
 type EthereumUnsignedTransaction = t.Static<typeof EthereumUnsignedTransaction>
 const EthereumUnsignedTransaction = t.Union(EthereumUnsignedTransactionLegacy, EthereumUnsignedTransaction2930, EthereumUnsignedTransaction1559)
@@ -91,51 +83,43 @@ const RevertErrorParser: t.ParsedValue<t.String, string>['config'] = {
 		const serializationResult = BytesParser.serialize!(encoded)
 		if (!serializationResult.success) return serializationResult
 		return { success: true, value: `Reverted ${serializationResult.value}` }
-	},
+	}
 }
 
 type MulticallResponseEventLog = t.Static<typeof MulticallResponseEventLog>
-const MulticallResponseEventLog = t
-	.Object({
-		loggersAddress: EthereumAddress,
-		data: EthereumInput,
-		topics: t.ReadonlyArray(EthereumBytes32),
-	})
-	.asReadonly()
+const MulticallResponseEventLog = t.Object({
+	loggersAddress: EthereumAddress,
+	data: EthereumInput,
+	topics: t.ReadonlyArray(EthereumBytes32),
+}).asReadonly()
 
 type MulticallResponseEventLogs = t.Static<typeof MulticallResponseEventLogs>
 const MulticallResponseEventLogs = t.ReadonlyArray(MulticallResponseEventLog)
 
 type EthBalanceChanges = t.Static<typeof EthBalanceChanges>
 const EthBalanceChanges = t.ReadonlyArray(
-	t
-		.Object({
-			address: EthereumAddress,
-			before: EthereumQuantity,
-			after: EthereumQuantity,
-		})
-		.asReadonly(),
+	t.Object({
+		address: EthereumAddress,
+		before: EthereumQuantity,
+		after: EthereumQuantity,
+	}).asReadonly()
 )
 
 type SingleMulticallResponse = t.Static<typeof SingleMulticallResponse>
 const SingleMulticallResponse = t.Union(
-	t
-		.Object({
-			statusCode: t.Literal(1).withParser(LiteralConverterParserFactory(1, 'success' as const)),
-			gasSpent: EthereumQuantity,
-			returnValue: EthereumData,
-			events: MulticallResponseEventLogs,
-			balanceChanges: EthBalanceChanges,
-		})
-		.asReadonly(),
-	t
-		.Object({
-			statusCode: t.Literal(0).withParser(LiteralConverterParserFactory(0, 'failure' as const)),
-			gasSpent: EthereumQuantity,
-			error: t.String.withParser(RevertErrorParser),
-			returnValue: EthereumData,
-		})
-		.asReadonly(),
+	t.Object({
+		statusCode: t.Literal(1).withParser(LiteralConverterParserFactory(1, 'success' as const)),
+		gasSpent: EthereumQuantity,
+		returnValue: EthereumData,
+		events: MulticallResponseEventLogs,
+		balanceChanges: EthBalanceChanges,
+	}).asReadonly(),
+	t.Object({
+		statusCode: t.Literal(0).withParser(LiteralConverterParserFactory(0, 'failure' as const)),
+		gasSpent: EthereumQuantity,
+		error: t.String.withParser(RevertErrorParser),
+		returnValue: EthereumData,
+	}).asReadonly()
 )
 
 export type GetSimulationStackReply = t.Static<typeof GetSimulationStackReply>
@@ -143,11 +127,9 @@ export const GetSimulationStackReply = t.ReadonlyArray(
 	t.Intersect(
 		EthereumUnsignedTransaction,
 		SingleMulticallResponse,
-		t
-			.Object({
-				realizedGasPrice: EthereumQuantity,
-				gasLimit: EthereumQuantity,
-			})
-			.asReadonly(),
-	),
+		t.Object({
+			realizedGasPrice: EthereumQuantity,
+			gasLimit: EthereumQuantity,
+		}).asReadonly()
+	)
 )
