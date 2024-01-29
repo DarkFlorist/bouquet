474825d856d951e025724838dd521b102f48e012
KillariDev <killari@gmail.com>
2024-01-29 08:43:17 +0200

    change to 🦫 and fix text

--- app/ts/constants.ts
+++ app/ts/constants.ts
@@ -1,4 +1,4 @@
 export const NETWORKS: { [chainId: string]: { simulationRelay: string, submissionRelay: string, blockExplorer: string, rpcUrl: string }} = {
-	'1': { simulationRelay: 'https://relay.dark.florist', submissionRelay: 'https://rpc.titanbuilder.xyz', blockExplorer: 'https://etherscan.io/', rpcUrl: 'https://rpc.dark.florist/flipcardtrustone' },
+	'1': { simulationRelay: 'https://relay.dark.florist', submissionRelay: 'https://beaver-cors-proxy.dark-florist.workers.dev', blockExplorer: 'https://etherscan.io/', rpcUrl: 'https://rpc.dark.florist/flipcardtrustone' },
 	'5': { simulationRelay: 'https://relay-goerli.dark.florist', submissionRelay: 'https://relay-goerli.dark.florist',  blockExplorer: 'https://goerli.etherscan.io/', rpcUrl: 'https://rpc-goerli.dark.florist/flipcardtrustone' }
 }
