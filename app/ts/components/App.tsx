27c31315523eaaeba41a9fdb0653340479e67ce4
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2024-07-08 23:27:35 +0800

    add welcome instructions

--- app/ts/components/App.tsx
+++ app/ts/components/App.tsx
@@ -18,7 +18,7 @@ export function App() {
 				<div className='p-4 mt-4 flex flex-col gap-8 w-full'>
 					{!state.provider.value ? (
 						<article className='items-center flex flex-col gap-4 py-8'>
-							<h2 class='text-2xl font-bold'>Welcome Back</h2>
+							<Greeting isReturningUser={Boolean(state.bundle.value)} />
 							<Button
 								onClick={() => connectBrowserProvider(state.provider, state.blockInfo, state.bundle.peek()?.containsFundingTx ? state.signers : undefined, state.appSettings)}
 							>
@@ -37,5 +37,21 @@ export function App() {
 				</div>
 			<Footer />
 		</main>
+	)
+}
+
+const Greeting = ({ isReturningUser }: { isReturningUser: boolean }) => {
+	if (isReturningUser) return (
+		<>
+			<h2 class='text-2xl font-bold'>Welcome Back</h2>
+			<p class='text-gray-500'>Connect your wallet to continue where you previously left off.</p>
+		</>
+	)
+
+	return (
+		<>
+			<h2 class='text-2xl font-bold'>Welcome</h2>
+			<p class='text-gray-500'>Let's start by connecting your wallet.</p>
+		</>
 	)
 }
